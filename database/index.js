fcebf0e53c53e77441719cd35fcf1d1c1e431725
Israel B. <brown.israel@outlook.com>
2025-10-27 07:52:07 -0500

    work

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const { Pool } = require("pg")
 require("dotenv").config()
-
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
@@ -11,25 +10,23 @@ let pool
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl:  false
   })
 
-  // Added for troubleshooting queries
-  // during development
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params)
-        console.log("executed query", { text })
-        return res
-      } catch (error) {
-        console.error("error in query", { text })
-        throw error
-      }
-    },
-  }
+// Added for troubleshooting queries
+// during development
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params)
+      console.log("executed query", { text })
+      return res
+    } catch (error) {
+      console.error("error in query", { text })
+      throw error
+    }
+  },
+}
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -7,6 +7,8 @@ async function getClassifications(){
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
+module.exports = {getClassifications}
+
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
  * ************************** */
@@ -24,20 +26,3 @@ async function getInventoryByClassificationId(classification_id) {
     console.error("getclassificationsbyid error " + error)
   }
 }
-
-/* ***************************
- *  Get inventory item details by inventory_id
- * ************************** */
-async function getInventoryByInventoryId(inv_id) {
-  try {
-    const data = await pool.query(
-      `SELECT * FROM public.inventory WHERE inv_id = $1`,
-      [inv_id]
-    )
-    return data.rows[0]
-  } catch (error) {
-    console.error("getInventoryByInventoryId error " + error)
-  }
-}
-
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInventoryId};
